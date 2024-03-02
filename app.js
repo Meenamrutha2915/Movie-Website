22e770b1104f587cfc99ec4b6472237b898a893c
Meenamrutha2915 <chippala.meenamrutha@gmail.com>
2024-03-01 22:37:56 -0800

    Change formatting

--- app.js
+++ app.js
@@ -48,77 +48,75 @@ signinForm.addEventListener('submit', async(event)=>{
 
 });
 
- //function to validate email format
- function isValidEmail(email){
+//function to validate email format
+function isValidEmail(email){
     // Regular expression for email format validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-  }
+}
 
-  //signUp form
-    signUpFormButton.addEventListener('click', async(event)=>{
+//signUp form
+signUpFormButton.addEventListener('click', async(event)=>{
     
-        event.preventDefault();
-        const username = document.getElementById("username").value;
-        const email = document.getElementById("regemail").value;
-        const password = document.getElementById("regpassword").value;
-        const confirmPassword = document.getElementById("confirmpsw").value;
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("regemail").value;
+    const password = document.getElementById("regpassword").value;
+    const confirmPassword = document.getElementById("confirmpsw").value;
 
-        if(!username||!email||!password||!confirmPassword){
-            return;
-        }
+    if(!username||!email||!password||!confirmPassword){
+        return;
+    }
 
-        if (password !== confirmPassword){
-            alert("wrong password");
-        }
+    if (password !== confirmPassword){
+        alert("wrong password");
+    }
 
-        try {
-            const userCredential = await firebase.auth().createUserWithEmailAndPassword(email,password);
-            const user = userCredential.user;
-            alert('Registration Sucessful');
-            document.getElementById('signupForm').reset();
-    
-        } catch (error) {
-            console.error(error);
-            alert('Registration Failed');
-        }
-    });
+    try {
+        const userCredential = await firebase.auth().createUserWithEmailAndPassword(email,password);
+        const user = userCredential.user;
+        alert('Registration Sucessful');
+        document.getElementById('signupForm').reset();
 
-    const key = "f06eba9f85f8ec39b59e5c3422e18617";
-    const apiURL = "https://api.themoviedb.org/3/search/movie?api_key=f06eba9f85f8ec39b59e5c3422e18617&query=";
-    const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-    
-    console.log('movieResults',movieResults);
+    } catch (error) {
+        console.error(error);
+        alert('Registration Failed');
+    }
+});
 
-    searchInput.addEventListener("keypress",async function(event){
+const key = "f06eba9f85f8ec39b59e5c3422e18617";
+const apiURL = "https://api.themoviedb.org/3/search/movie?api_key=f06eba9f85f8ec39b59e5c3422e18617&query=";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+searchInput.addEventListener("keypress", async function(event){
     if (event.key === "Enter") {
         event.preventDefault()
-            searchMovies();
-        }
-    });
+        searchMovies();
+    }
+});
     
-   async function searchMovies(){
-        try {
-            const response = await fetch(apiURL + searchInput.value);
-            const moviesdata = await response.json();
-            console.log( 'Movies data :' ,moviesdata);
-            const movies = moviesdata.results;
+async function searchMovies() {
+    try {
+        const response = await fetch(apiURL + searchInput.value);
+        const moviesdata = await response.json();
+        console.log( 'Movies data :' ,moviesdata);
+        const movies = moviesdata.results;
 
-            const html = movies.map(movie =>
-                `<div class="card" style="width: 18rem;">
-                    <img src="${IMG_PATH + movie.poster_path}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${movie.title}</h5>
-                        <p class="card-text">${movie.overview}</p>
-                        <p class="card-text">${movie.vote_count}</p>
-                        <p class ="card-text">${movie.release_date}</p>
-                    </div>
-                </div>`);
-                
+        const html = movies.map(movie =>
+            `<div class="card" style="width: 18rem;">
+                <img src="${IMG_PATH + movie.poster_path}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${movie.title}</h5>
+                    <p class="card-text">${movie.overview}</p>
+                    <p class="card-text">${movie.vote_count}</p>
+                    <p class ="card-text">${movie.release_date}</p>
+                </div>
+            </div>`);
+            
 
-            movieResults.innerHTML = html;
+        movieResults.innerHTML = html;
 
-           } catch(error) {
+    } catch(error) {
         console.log("error ", error);
     }
 }
